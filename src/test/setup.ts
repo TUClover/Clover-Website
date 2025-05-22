8af81d87ed9d64c7341ef226cc39ae98f7b9fca9
Nicholas Rucinski <nrucinski01@gmail.com>
2025-05-22 13:27:11 -0400

    Updates

--- src/test/setup.ts
+++
@@ -1,2 +0,0 @@
-// setup.ts
-import "@testing-library/jest-dom";
--- src/utils/timeConverter.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts a date string to a Date object.
- * @param isoString - The ISO date string to be parsed.
- * @returns {Date} - The parsed date object.
- */
-export const parseISODate = (isoString: string) => {
-  const date = new Date(isoString);
-  return new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-};
