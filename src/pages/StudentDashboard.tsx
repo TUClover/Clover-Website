baec93f6079c20cc546fffff1acbda754b8927e2
Nicholas Rucinski <nrucinski01@gmail.com>
2025-06-18 10:51:54 -0400

    added a button to the student card to link to the download page

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -59,13 +59,21 @@ export const StudentDashboard = ({ userData }: { userData: UserData }) => {
 
   if (progressData.totalAccepted === 0) {
     return (
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex justify-center">
         <div className="text-center card">
           <h2 className="text-lg font-semibold text mb-4">No activity found</h2>
           <p className="text-gray-500">
             It seems you haven't accepted any suggestions yet. Start coding to
             see your progress here!
           </p>
+          <a
+            href="https://clover.nickrucinski.com/download"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 inline-block bg-blue-400 dark:bg-blue-500 hover:bg-blue-500 dark:hover:bg-blue-700 hover:scale-105 text-white font-bold py-2 px-4 rounded transition-all"
+          >
+            Download CLOVER
+          </a>
         </div>
       </div>
     );
