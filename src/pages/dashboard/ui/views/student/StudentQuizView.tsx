2027ddf417fe3254968d1eff819e983a3c9d7330
Nicholas Rucinski <nrucinski01@gmail.com>
2025-07-22 13:06:12 -0400

    added the quiz component to the new page. Need to restyle it

--- src/pages/dashboard/ui/views/student/StudentQuizView.tsx
+++ src/pages/dashboard/ui/views/student/StudentQuizView.tsx
@@ -1,5 +1,12 @@
+import { Card } from "@/components/ui/card";
+import QuizPage from "@/pages/Quiz";
+
 const StudentQuizView = () => {
-  return <div></div>;
+  return (
+    <Card className="p-6">
+      <QuizPage />
+    </Card>
+  );
 };
 
 export default StudentQuizView;
