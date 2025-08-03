8c9f101ee50e7619b1b9af8e60d7f0fbabbf8575
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2025-08-03 13:42:46 -0400

    Reset password UI

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,6 +1,7 @@
 /** Gets the endpoint from the .env if it is available otherwise it uses localhost */
 export const BASE_ENDPOINT =
   import.meta.env.VITE_API_URL || "http://127.0.0.1:8080/api/v1";
+
 /* Endpoint for creating new AI suggestions */
 export const AI_SUGGESTION_ENDPOINT: string = `${BASE_ENDPOINT}/suggestion`;
 
@@ -19,3 +20,7 @@ export const AUTH_ENDPOINT: string = `${BASE_ENDPOINT}/auth`;
 export const CLASS_ENDPOINT: string = `${BASE_ENDPOINT}/classes`;
 
 export const IMAGE_UPLOAD_ENDPOINT = `${BASE_ENDPOINT}/images/upload`;
+
+export const RESET_PASSWORD_REDIRECT = import.meta.env.VITE_API_URL
+  ? "https://clover.nickrucinski.com/resetform"
+  : "http://localhost:5173/resetform";
